84fc11453a0f2e60b22593970aefe5b1876ea20d
Princess adu <princess.m.adu@student.uncommoncollegiate.org>
2017-02-28 22:24:20 +0000

    changes

---
+++ script.js
@@ -0,0 +1,10 @@
+var turtle = TinyTurtle.apply(window); 
+turtle.stamp();
+turtle.forward(40);
+turtle.right(90);
+turtle.forward(40);
+turtle.right(90);
+turtle.stamp()
+turtle.forward(40);
+turtle.right(90);
+turtle.forward(40);
